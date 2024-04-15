9169047d6c22d127967a916c389c4a6bba8017fd
dantt25 <dantt@avadagroup.com>
2024-04-15 13:15:18 +0700

    [wip]

--- pin-fe/src/components/molecules/GeneralOptions/Options/TextSettings.tsx
+++ pin-fe/src/components/molecules/GeneralOptions/Options/TextSettings.tsx
@@ -1,6 +1,15 @@
 import { useContext } from "react";
+import { EditorPanelContext } from "../../../../contexts/EditorPanelContext";
 
 export default function TextSettings() {
-  const {input} = useContext(EditorPanelContext)
+  const {input} = useContext(EditorPanelContext);
+
+  const textOptions = [
+    {
+      label: "Text color",
+      key: ""
+    }
+  ]
+
 
 }
