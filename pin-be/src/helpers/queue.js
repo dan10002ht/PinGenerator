1973f41804399a22286bc0d7aff919cc39ab3c67
dantt2 <dantt1002@gmail.com>
2024-05-03 00:45:39 +0700

    change queue a bit

--- pin-be/src/helpers/queue.js
+++ pin-be/src/helpers/queue.js
@@ -1,7 +1,7 @@
 import {Queue, RedisConnection} from 'bullmq';
 
 const connection = new RedisConnection({
-  host: 'locahost',
+  host: 'localhost',
   port: 6379
 });
 
@@ -9,7 +9,7 @@ const queue = new Queue(PUBLISH_PIN_EVENTS, {connection});
 
 export const addJob = async (queueName, data) => {
   try {
-    await queue.add(queueName, data)
+    await queue.add(queueName, data);
   } catch (e) {
     console.log(e);
   }
