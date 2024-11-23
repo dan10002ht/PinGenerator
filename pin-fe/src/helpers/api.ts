bf52230ce5494cfede5f0303511bd8827590a844
dantt25 <dantt@avadagroup.com>
2024-11-23 10:59:44 +0700

    add env

--- pin-fe/src/helpers/api.ts
+++ pin-fe/src/helpers/api.ts
@@ -16,7 +16,7 @@ export async function api({
   params = {},
   options = {},
   clientConfig = {
-    baseURL: 'http://localhost:4000/api',
+    baseURL: `${import.meta.env.VITE_API_URL}/api`,
     timeout: 60000,
   },
 }: {
